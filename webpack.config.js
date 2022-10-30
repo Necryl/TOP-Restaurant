a62076db897f88555b1c5f4dc13e852ada26c55e
Necryl <necrylthewayfarer@gmail.com>
2022-10-30 08:59:33 +0530

    renamed some files to follow the instructions from TOP

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const path = require("path");
 module.exports = {
     mode: 'development',
     entry: {
-        main: path.resolve(__dirname, "src/script.js"),
+        main: path.resolve(__dirname, "src/index.js"),
     },
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "[name].bundle.js",
-        clean: true,
+        filename: "main.js",
+        // clean: true,
     }
 }
