0145dc26b2511946441a09c48884b2b1a840958c
Necryl <necrylthewayfarer@gmail.com>
2022-10-31 05:48:27 +0530

    add css loaders and use them in webpack.config.js and import style.css in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import initiate from './initiate.js'
+import style from './style.css'
 initiate();
 console.log("Hello");
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        main: path.resolve(__dirname, "src/index.js"),
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Restaurant: TOP Project",
-            filename: "index.html",
-        }),
-    ],
-}
+  mode: "development",
+  entry: {
+    main: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true,
+  },
+  module: {
+    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Restaurant: TOP Project",
+      filename: "index.html",
+    }),
+  ],
+};
